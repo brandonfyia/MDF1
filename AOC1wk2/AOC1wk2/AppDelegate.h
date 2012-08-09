00b3842ed075150462f33131f9605a2f99be182e
Brandon Sease <brandonfyia@gmail.com>
2012-08-09 19:50:14 -0400

    Setting up week 2

---
+++ AOC1wk2/AOC1wk2/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AOC1wk2
+//
+//  Created by Brandon Sease on 8/9/12.
+//  Copyright (c) 2012 Brandon Sease. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
