454a066c6e607d1717d08bac85c38190e2a1a42a
Brandon Sease <brandonfyia@gmail.com>
2012-08-16 21:48:08 -0400

    realized i havnt been saving. 1-3 done.

---
+++ AOC1wk3/AOC1wk3/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AOC1wk3
+//
+//  Created by Brandon Sease on 8/16/12.
+//  Copyright (c) 2012 Brandon Sease. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
