c518a7994ebb05ed5075be5a0f04ec2965ed3288
Brandon Sease <brandonfyia@gmail.com>
2012-09-26 20:13:39 -0400

    folowing the video. got the base class sorted out for the moment. starting sub classes.

---
+++ AOC2_Wk1/AOC2_Wk1/baseGas.h
@@ -0,0 +1,13 @@
+//
+//  baseGas.h
+//  AOC2_Wk1
+//
+//  Created by Brandon Sease on 9/26/12.
+//  Copyright (c) 2012 Brandon Sease. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface baseGas : NSObject
+
+@end
