d7f81a156d75ffc48c1776e2d053cd8535de9eb5
Brandon Sease <brandonfyia@gmail.com>
2012-10-04 21:22:13 -0400

    Started wk 2. layed out visual half of calc. about to start code.

---
+++ AOC2_Wk2/AOC2_Wk2/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AOC2_Wk2
+//
+//  Created by Brandon Sease on 10/4/12.
+//  Copyright (c) 2012 Brandon Sease. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
