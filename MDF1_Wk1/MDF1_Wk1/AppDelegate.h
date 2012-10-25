354e96caafc1d204d93fd4dc13f6dbb4fe034dd1
Brandon Sease <brandonfyia@gmail.com>
2012-10-24 21:27:02 -0400

    Created wk 1 project.

---
+++ MDF1_Wk1/MDF1_Wk1/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  MDF1_Wk1
+//
+//  Created by Brandon Sease on 10/24/12.
+//  Copyright (c) 2012 Brandon Sease. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ MDF1_Wk1/MDF1_Wk1/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MDF1_Wk1
+//
+//  Created by Brandon Sease on 10/24/12.
+//  Copyright (c) 2012 Brandon Sease. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
