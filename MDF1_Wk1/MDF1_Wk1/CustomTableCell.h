7ba221a9fd9b94137d7f1044e34de6bec873d82d
Brandon Sease <brandonfyia@gmail.com>
2012-10-25 02:06:30 -0400

    Working on custom cells. Getting crash.

---
+++ MDF1_Wk1/MDF1_Wk1/CustomTableCell.h
@@ -0,0 +1,13 @@
+//
+//  CustomTableCell.h
+//  MDF1_Wk1
+//
+//  Created by Brandon Sease on 10/25/12.
+//  Copyright (c) 2012 Brandon Sease. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomTableCell : UITableViewCell
+
+@end
---
+++ MDF1_Wk1/MDF1_Wk1/CustomTableCell.m
@@ -0,0 +1,29 @@
+//
+//  CustomTableCell.m
+//  MDF1_Wk1
+//
+//  Created by Brandon Sease on 10/25/12.
+//  Copyright (c) 2012 Brandon Sease. All rights reserved.
+//
+
+#import "CustomTableCell.h"
+
+@implementation CustomTableCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
