53006d4b985aa5cd726970fc4eb6527aeff10cbc
Brandon Sease <brandonfyia@gmail.com>
2012-10-30 21:14:39 -0400

    Created a detail page. Layout finished just need to code it all together.

---
+++ MDF1_Wk2/MDF1_Wk2/DetailViewController.m
@@ -0,0 +1,38 @@
+//
+//  DetailViewController.m
+//  MDF1_Wk2
+//
+//  Created by Brandon Sease on 10/30/12.
+//  Copyright (c) 2012 Brandon Sease. All rights reserved.
+//
+
+#import "DetailViewController.h"
+
+@interface DetailViewController ()
+
+@end
+
+@implementation DetailViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
